dad5e61f2d0700e71c2f8c8c364857cdb2cd7f3a
Peter Hancock <hancockp@uk.ibm.com>
2020-08-17 14:37:35 +0100

    init commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ARCH=$(uname -m)
+git clone https://github.com/travis-ci/travis-build.git
+cd ./travis-build
+git checkout -t origin/enterprise-2.2
+rm -f ./Dockerfile
+cp ../Dockerfile.s390x ./Dockerfile
+docker build -t quay.io/rpsene/travis:build .
+docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
+docker images
+docker push quay.io/rpsene/travis:build
