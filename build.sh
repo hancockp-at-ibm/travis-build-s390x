3385a7205b63542c8acb6b7d146bd1a33fe02206
Peter Hancock <hancockp@uk.ibm.com>
2020-08-18 12:15:07 +0100

    change repo 3

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd ./travis-build
 git checkout -t origin/enterprise-2.2
 rm -f ./Dockerfile
 cp ../Dockerfile.s390x ./Dockerfile
-docker build -t quay.io/hancockp/travis/travis-build-ee-s390x:d20200818 .
+docker build -t quay.io/hancockp/travis:travis-build-ee-s390x-20200818 .
 docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
 docker images
-docker push quay.io/hancockp/travis/travis-build-ee-s390x:d20200818
+docker push quay.io/hancockp/travis:travis-build-ee-s390x-20200818
