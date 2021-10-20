aac533974cfb481497a85890746b02580248fe94
hancockp-at-ibm <31920724+hancockp-at-ibm@users.noreply.github.com>
2021-10-20 11:35:41 +0100

    set date to today

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd ./travis-build
 git checkout -t origin/enterprise-2.2
 rm -f ./Dockerfile
 cp ../Dockerfile.s390x ./Dockerfile
-docker build -t quay.io/hancockp/travis:travis-build-ee-s390x-20211018 .
+docker build -t quay.io/hancockp/travis:travis-build-ee-s390x-20211020 .
 docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
 docker images
-docker push quay.io/hancockp/travis:travis-build-ee-s390x-20211018
+docker push quay.io/hancockp/travis:travis-build-ee-s390x-20211020
