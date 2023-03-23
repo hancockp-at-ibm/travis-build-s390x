2fdcd491dbccec2b75b8bfd3c46233a13372dfd8
hancockp-at-ibm <31920724+hancockp-at-ibm@users.noreply.github.com>
2023-03-23 22:23:14 +0000

    chore: Update for travis 3

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 ARCH=$(uname -m)
 git clone https://github.com/travis-ci/travis-build.git
 cd ./travis-build
-git checkout -t origin/enterprise-2.2
+git checkout -t origin/enterprise-3.0
 rm -f ./Dockerfile
 cp ../Dockerfile.s390x ./Dockerfile
-docker build -t quay.io/hancockp/travis:travis-build-ee-s390x-20211020 .
+docker build -t quay.io/hancockp/travis:travis3-build-ee-s390x-20230323 .
 docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
 docker images
-docker push quay.io/hancockp/travis:travis-build-ee-s390x-20211020
+docker push quay.io/hancockp/travis:travis3-build-ee-s390x-20230323
